4aa14ad1f697887e9c6e91ad50d3c591911ae86f
ArdaBarman <barmanardaz@gmail.com>
2023-08-08 17:42:44 +0300

    Update main.dart
    changed the place of the const

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:rfirst_project/gradient_container.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+     const MaterialApp(
       home: Scaffold(
         body: GradientContainer(
-          const Color.fromARGB(198, 96, 255, 173),
-          const Color.fromARGB(255, 33, 243, 236),
+           Color.fromARGB(198, 96, 255, 173),
+           Color.fromARGB(255, 33, 243, 236),
         ),
       ),
     ),
